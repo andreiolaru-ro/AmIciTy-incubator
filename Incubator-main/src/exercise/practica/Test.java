030eac6e5c8e4b96d45b835d32e49fc084088d6f
George Cirlan <george.cirlan@gmail.com>
2013-07-15 14:04:23 +0300

    My First Commit !

---
+++ Incubator-main/src/exercise/practica/Test.java
@@ -0,0 +1,14 @@
+package exercise.practica;
+
+/**
+ * @author ''George Cirlan''
+ *
+ */
+public class Test {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ceva");
+	}
+}
