f9bf327b0d4aa2a9ea337d6b1e985c81c2172572
Azgabast <obelisk_tt@yahoo.com>
2013-08-06 13:12:25 +0300

    Added pc communication files to my incubator project.

--- Incubator-main/src/net/amicity/communications/Connection.java
+++ Incubator-main/src/net/amicity/communications/Connection.java
@@ -1,4 +1,5 @@
 package net.amicity.communications;
+
 /*******************************************************************************
  * Copyright (c) 2013 ''Azgabast''.
  * All rights reserved. This program and the accompanying materials
