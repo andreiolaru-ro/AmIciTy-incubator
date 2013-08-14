55b93652dbdff857afabad8db62301d7f6d5c6c9
Azgabast <obelisk_tt@yahoo.com>
2013-08-14 15:02:38 +0300

    Added javadoc to SoundModule.

--- AmIciTy-incubator-Android/src/net/amicity/android/sensors/SoundModule.java
+++ AmIciTy-incubator-Android/src/net/amicity/android/sensors/SoundModule.java
@@ -7,10 +7,22 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+/**
+ * The class which listens periodically to ambient sounds and creates a
+ * context item based on the level of the sound.
+ * @author ''Azgabast''
+ *
+ */
 public class SoundModule extends Service implements SensorModule
 {
 
+	/**
+	 * The core linked with the module.
+	 */
 	ContextCore ctxCore;
+	/**
+	 * The sound item.
+	 */
 	ContextItem ctxItem;
 	@Override
 	public void connect(ContextCore cc)
