1af814eeddaeec919a77f2162a616222324fbcab
Azgabast <obelisk_tt@yahoo.com>
2013-09-05 13:37:48 +0300

    Added getters for members of PerceptionItem. Posted the accelerometer and sound results in the interface.

--- Incubator-main/src/net/amicity/common/context_types/PerceptionItem.java
+++ Incubator-main/src/net/amicity/common/context_types/PerceptionItem.java
@@ -1,42 +1,59 @@
 package net.amicity.common.context_types;
 
-
 /**
  * A class for sending perceptions
+ * 
  * @author cristian
- *
+ * 
  */
 public class PerceptionItem extends AbstractItem {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * The accelerometer perception
 	 */
 	String action;
-	
+
 	/**
 	 * The sound perception
 	 */
 	double value;
-	
+
 	/**
 	 * The user that send perceptions
 	 */
 	String user;
-	
+
 	/**
-	 * @param user the user that sends
-	 * @param value the value of sound perception
-	 * @param action the action from accelerometer perception
+	 * @param user
+	 *            the user that sends
+	 * @param value
+	 *            the value of sound perception
+	 * @param action
+	 *            the action from accelerometer perception
 	 */
 	public PerceptionItem(String user, double value, String action) {
 		this.user = user;
 		this.value = value;
 		this.action = action;
+	}
+
+	/**
+	 * @return Whether the user is standing or walking.
+	 */
+	public String getAction() {
+		return this.action;
+	}
+
+	/**
+	 * @return The current value of the ambient sound.
+	 */
+	public double getValue() {
+		return this.value;
 	}
 
 }
