eb4e96ea2a742f51ec6d56aeb552bee960fee366
Grigoras Cristian <grigoras.cristian92@gmail.com>
2013-09-10 11:55:32 +0300

    Adding working files

---
+++ Incubator-main/munca/ServerModule.java
@@ -0,0 +1,44 @@
+nullpackage net.amicity.pc.sensors;
+
+import java.net.Socket;
+
+import net.amicity.common.core.SensorModule;
+import net.amicity.common.core.context.ContextCore;
+import net.amicity.pc.communications.DefaultMessageReceiver;
+import net.amicity.pc.communications.DefaultNetLink;
+
+
+/**
+ * @author cristian
+ *
+ */
+public class ServerModule implements SensorModule {
+	
+	/**
+	 * instance of ContextCore to acces its queues
+	 */
+	ContextCore myCore;
+	/**
+	 * the server socket
+	 */
+	private Socket server;
+	
+	/**
+	 * @param server the server socket
+	 */
+	public ServerModule(Socket server) {
+		this.server = server;
+	}
+
+	@Override
+	public void connect(ContextCore cc) {
+		myCore = cc;
+		//listen from server:
+		DefaultNetLink d = new DefaultNetLink();
+		d.receiveFromServer(server, new DefaultMessageReceiver());
+		//listen from other devices
+		DefaultNetLink d2 = new DefaultNetLink();
+		d2.initializeReceival(4501, new DefaultMessageReceiver());
+	}
+	
+}
---
+++ Incubator-main/munca/WirelessModule.java
@@ -0,0 +1,125 @@
+null/*******************************************************************************
+ * Copyright (C) 2013 Andrei Olaru , Vlad Herescu, Cristian Radu Neagoe, Cristian Grigoras.
+ * 
+ * This file is part of AmIciTy-incubator.
+ * 
+ * AmIciTy-incubator is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * 
+ * AmIciTy-incubator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with AmIciTy-incubator.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.amicity.pc.sensors;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import net.amicity.common.context_types.AbstractItem;
+import net.amicity.common.context_types.WirelessItem;
+import net.amicity.common.core.SensorModule;
+import net.amicity.common.core.context.ContextCore;
+
+/**
+ * 
+ * detects wireless networks
+ * 
+ * @author vlad
+ * 
+ */
+public class WirelessModule implements SensorModule {
+
+	/**
+	 * a ContextItem which will be added to core's queue
+	 */
+	AbstractItem wirelessItem;
+
+	/**
+	 * the location detected used further for connection
+	 */
+	String location;
+
+	/**
+	 * instance of ContextCore to acces its queues
+	 */
+	ContextCore myCore;
+
+	/**
+	 * constructor of the class initialize its members
+	 */
+	public WirelessModule() {
+
+		wirelessItem = new WirelessItem();
+		System.out.println(" wirelessModule constructor ");
+
+	}
+
+	@Override
+	public void connect(ContextCore coreReceived) {
+		myCore = coreReceived;
+		System.out.println(" wireless module connect");
+		obtainData();
+		System.out.println(" wireless module obtainData");
+	}
+
+	/**
+	 * 
+	 */
+	public void obtainData() {
+
+		ProcessBuilder builder = new ProcessBuilder("netsh", "wlan", "show",
+				"networks");
+
+		Process process;
+		try {
+			process = builder.start(); // pornesc programul
+			InputStream is = process.getInputStream(); // obtin outuputul
+														// shellului ca input in
+														// program
+			// clasa este abstracta
+			InputStreamReader isr = new InputStreamReader(is); // creez o
+																// instanta ISR
+																// pe baza lui
+																// InputStream
+			BufferedReader br = new BufferedReader(isr); // transform streamul
+															// de biti in
+															// caractere
+			String line;
+			while ((line = br.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line, ": ");
+				while (st.hasMoreTokens()) {
+					String nameLine = st.nextToken();
+					if (nameLine.compareTo("SSID") == 0) {
+						st.nextToken();
+						((WirelessItem) wirelessItem).wifiDetected.add(st
+								.nextToken());
+
+					}
+					else
+						break;
+				}
+			}
+			br.close();
+
+		}
+		catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		addDataDetected();
+
+	}
+
+	/**
+	 * add data collected to ContextUpdates
+	 */
+	public void addDataDetected() {
+
+		ContextCore.postContextUpdate(wirelessItem);
+
+	}
+
+}
